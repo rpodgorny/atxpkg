85a41286d682947fd7b4f1c4016fca81972caa97
Radek Podgorny <radek@pogorny.cz>
2014-04-10 12:07:56 +0200

    new compile script

--- compile.bat
+++ compile.bat
@@ -1,6 +1,8 @@
+echo this is compile.bat v0.2
+
 setlocal
 
-set name=atxpkg
+set name=router
 set PYTHONPATH=../pylib;../libsh
 
 rd /s /q build
@@ -10,8 +12,8 @@ python setup.py install --prefix=dist
 
 rd /s /q pkg
 md pkg
-md pkg\%name%
-cp -av dist/* pkg/%name%/
+md pkg\atx300\%name%
+cp -av dist/* pkg/atx300/%name%/
 cp atxpkg_backup pkg/.atxpkg_backup
 
 ;rem rd /s /q build
@@ -20,11 +22,26 @@ cp atxpkg_backup pkg/.atxpkg_backup
 set /p version=<.version
 rm .version
 
+;rem no, i can't do this inside of the if for some reason - FUCK WINDOWS!
 awk "BEGIN {print strftime(\"%%Y%%m%%d%%H%%M%%S\")}" >.datetime
 set /p datetime=<.datetime
 rm .datetime
 
-set pkg_fn=%name%-%version%a%datetime%.atxpkg.zip
+if "%1" == "" (
+	;rem awk "BEGIN {print strftime(\"%%Y%%m%%d%%H%%M%%S\")}" >.datetime
+	;rem set /p datetime=<.datetime
+	;rem rm .datetime
+
+	echo devel version %datetime%
+	set version=%version%a%datetime%
+) else if "%1" == "release" (
+	echo release version
+) else (
+	echo unknown parameter!
+	goto end
+)
+
+set pkg_fn=%name%-%version%.atxpkg.zip
 
 rm %pkg_fn%
 
@@ -35,3 +52,5 @@ cd ..
 ;rem rd /s /q pkg
 
 pscp %pkg_fn% radek@podgorny.cz:public_html/atxpkg/
+
+:end
