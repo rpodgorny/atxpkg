0e11e59dbc76c11595c17afbd5d745737313ae56
Radek Podgorny <radek@podgorny.cz>
2015-01-30 14:58:34 +0100

    better parsing

--- test.py
+++ test.py
@@ -5,12 +5,22 @@ import unittest
 from utils import *
 
 class MyTestCase(unittest.TestCase):
-	def test_package_version(self):
-		self.assertEqual(get_package_version('package-3.5.6-1.atxpkg.zip'), '3.5.6-1')
+	def test_package_name(self):
+		self.assertEqual(get_package_name('package-3.5.6-1.atxpkg.zip'), 'package')
+		self.assertEqual(get_package_name('package-3.5.6-1'), 'package')
+		self.assertEqual(get_package_name('package'), 'package')
+
+		self.assertEqual(get_package_name('package-name-3.5.6-1.atxpkg.zip'), 'package-name')
+		self.assertEqual(get_package_name('package-name-3.5.6-1'), 'package-name')
+		self.assertEqual(get_package_name('package-name'), 'package-name')
 	#enddef
 
-	def test_package_version_hyphen(self):
+	def test_package_version(self):
+		self.assertEqual(get_package_version('package-3.5.6-1.atxpkg.zip'), '3.5.6-1')
 		self.assertEqual(get_package_version('package-with-hyphen-3.5.6-1.atxpkg.zip'), '3.5.6-1')
+
+		self.assertEqual(get_package_version('package'), None)
+		self.assertEqual(get_package_version('package-with-hyphen'), None)
 	#enddef
 
 	def test_valid_package_fn(self):
@@ -19,10 +29,24 @@ class MyTestCase(unittest.TestCase):
 		self.assertTrue(is_valid_package_fn('package_name-3.5.6-6.atxpkg.zip'))
 		self.assertTrue(is_valid_package_fn('package-name.dev-3.5.6-6.atxpkg.zip'))
 		self.assertTrue(is_valid_package_fn('package-name-3-6.atxpkg.zip'))
+		self.assertTrue(is_valid_package_fn('package.dev-20150101145536-6.atxpkg.zip'))
+		self.assertTrue(is_valid_package_fn('package-name.dev-20150101145536-6.atxpkg.zip'))
 
 		self.assertFalse(is_valid_package_fn('package-xxx-6.atxpkg.zip'))
 		self.assertFalse(is_valid_package_fn('package-name-xxx-xxx.atxpkg.zip'))
 		self.assertFalse(is_valid_package_fn('package-name-3-6.xxx.zip'))
+	#enddef
+
+	def test_max_version(self):
+		urls = [
+			'http://example.com/repo/package-2.2-3.atxpkg.zip',
+			'http://example.com/repo/package-1.2-3.atxpkg.zip',
+			'http://example.com/repo/package-1.2-4.atxpkg.zip',
+			'http://example.com/repo/package-1.2222-4.atxpkg.zip',
+			'http://example.com/repo/package-1.2222-44444.atxpkg.zip',
+		]
+
+		self.assertEqual(get_max_version_url(urls), 'http://example.com/repo/package-2.2-3.atxpkg.zip')
 	#enddef
 #endclass
 
--- utils.py
+++ utils.py
@@ -1,22 +1,44 @@
 import re
+from distutils.version import LooseVersion
 
+def get_max_version_url(urls):
+	return sorted(urls, key=lambda x: LooseVersion(get_package_version(get_package_fn(x))))[-1]
+#enddef
 
 def get_package_fn(url):
 	return url.split('/')[-1]
 #enddef
 
+# TODO: fn is not really fn here
 def get_package_name(fn):
 	fn = re.sub('\.atxpkg\..*', '', fn)
-	name, ver, rel = fn.rsplit('-', 2)
-	return name
+
+	# TODO: this is actually not really nice
+	try:
+		name, ver, rel = fn.rsplit('-', 2)
+		return name
+	except:
+		return fn
+	#endtry
 #enddef
 
+# TODO: fn is not really fn here
 def get_package_version(fn):
 	fn = re.sub('\.atxpkg\..*', '', fn)
-	name, ver, rel = fn.rsplit('-', 2)
-	return '%s-%s' % (ver, rel)
+
+	# TODO: this is actually not really nice
+	if has_version(fn):
+		name, ver, rel = fn.rsplit('-', 2)
+		return '%s-%s' % (ver, rel)
+	else:
+		return None
+	#endtry
 #enddef
 
 def is_valid_package_fn(fn):
 	return re.match('[\w\-\.]+-[\d.]+-\d+\.atxpkg\.zip', fn) is not None
+#enddef
+
+def has_version(pkg_spec):
+	return re.match('[\w\-\.]+-[\d.]+-\d+', pkg_spec) is not None
 #enddef
