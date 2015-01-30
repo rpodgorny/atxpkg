ae9c9ff094fe18d3ee3639b640ffd11849f0420a
Radek Podgorny <radek@podgorny.cz>
2015-01-30 13:45:17 +0100

    validate package filename

--- test.py
+++ test.py
@@ -12,6 +12,18 @@ class MyTestCase(unittest.TestCase):
 	def test_package_version_hyphen(self):
 		self.assertEqual(get_package_version('package-with-hyphen-3.5.6-1.atxpkg.zip'), '3.5.6-1')
 	#enddef
+
+	def test_valid_package_fn(self):
+		self.assertTrue(is_valid_package_fn('package-3.5.6-6.atxpkg.zip'))
+		self.assertTrue(is_valid_package_fn('package-name-3.5.6-6.atxpkg.zip'))
+		self.assertTrue(is_valid_package_fn('package_name-3.5.6-6.atxpkg.zip'))
+		self.assertTrue(is_valid_package_fn('package-name.dev-3.5.6-6.atxpkg.zip'))
+		self.assertTrue(is_valid_package_fn('package-name-3-6.atxpkg.zip'))
+
+		self.assertFalse(is_valid_package_fn('package-xxx-6.atxpkg.zip'))
+		self.assertFalse(is_valid_package_fn('package-name-xxx-xxx.atxpkg.zip'))
+		self.assertFalse(is_valid_package_fn('package-name-3-6.xxx.zip'))
+	#enddef
 #endclass
 
 
--- utils.py
+++ utils.py
@@ -5,16 +5,18 @@ def get_package_fn(url):
 	return url.split('/')[-1]
 #enddef
 
-
 def get_package_name(fn):
 	fn = re.sub('\.atxpkg\..*', '', fn)
 	name, ver, rel = fn.rsplit('-', 2)
 	return name
 #enddef
 
-
 def get_package_version(fn):
 	fn = re.sub('\.atxpkg\..*', '', fn)
 	name, ver, rel = fn.rsplit('-', 2)
 	return '%s-%s' % (ver, rel)
+#enddef
+
+def is_valid_package_fn(fn):
+	return re.match('[\w\-\.]+-[\d.]+-\d+\.atxpkg\.zip', fn) is not None
 #enddef
