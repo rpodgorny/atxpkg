62fe012fe8b53f91274344d9369b291ab26bc138
Radek Podgorny <radek@podgorny.cz>
2015-02-19 12:27:21 +0100

    logging refactored

--- utils.py
+++ utils.py
@@ -1,5 +1,48 @@
+import sys
+import logging
+import datetime
 import re
 from distutils.version import LooseVersion
+
+
+# TODO: this is cut-n-pasted from atxutils - decouple from atx
+def logging_setup(level, fn=None, print_=True):
+	sys.excepthook = lambda type, value, traceback: logging.critical('unhandled exception', exc_info=(type, value, traceback))
+
+	logger = logging.getLogger()
+	logger.setLevel(logging.DEBUG)
+
+	class AtxFormatter(logging.Formatter):
+		def formatTime(self, record, datefmt=None):
+			ct = datetime.datetime.fromtimestamp(record.created)
+			if datefmt:
+				s = ct.strftime(datefmt)
+			else:
+				s = ct.strftime('%Y-%m-%d %H:%M:%S.%f')
+			#endif
+			return s
+		#enddef
+	#endclass
+
+	formatter = AtxFormatter('%(asctime)s: %(levelname)s: %(message)s')
+
+	if print_:
+		sh = logging.StreamHandler()
+		sh.setLevel(level)
+		sh.setFormatter(formatter)
+		logger.addHandler(sh)
+	else:
+		nh = logging.NullHandler()
+		logger.addHandler(nh)
+	#endif
+
+	if fn:
+		fh = logging.FileHandler(fn)
+		fh.setLevel(level)
+		fh.setFormatter(formatter)
+		logger.addHandler(fh)
+	#endif
+#enddef
 
 def get_max_version_url(urls):
 	map_ = {get_package_version(get_package_fn(url)): url for url in urls}
