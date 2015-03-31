e288bfb48df25ee5046c0fc5db1e71054b0ab661
Radek Podgorny <radek@podgorny.cz>
2015-03-31 16:25:16 +0200

    better add_to_path.bat

--- add_to_path.bat
+++ add_to_path.bat
@@ -1,8 +1,9 @@
 set p=c:\atxpkg
 
 Echo.%PATH% | findstr /C:"%p%">nul && (
-	echo %p% already in path
+  echo %p% already in path
 ) || (
-	echo adding to %p% to path
-	atxpkg_setx PATH "%PATH%;%p%" -m
+  echo adding to %p% to path
+  atxpkg_setx PATH "%PATH%;%p%" -m
+  set "PATH=%PATH%;%p%"
 )
