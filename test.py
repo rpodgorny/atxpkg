eb3e45fe884446a82f06652a794c4be54581c176
Radek Podgorny <radek@podgorny.cz>
2016-06-18 18:39:52 +0200

    pep8 formatting

--- test.py
+++ test.py
@@ -23,20 +23,15 @@ class HighLevelTestCase(unittest.TestCase):
 		with open('%s/repos.txt' % self.d, 'w') as f:
 			f.write('http://atxpkg.asterix.cz\n')
 			f.write('http://atxpkg-dev.asterix.cz\n')
-		#endwith
-	#endef
 
 	def tearDown(self):
 		if os.path.isdir(self.d): shutil.rmtree(self.d)
 		if os.path.isdir('/tmp/atxpkg_dest'): shutil.rmtree('/tmp/atxpkg_dest')
-	#enddef
 
 	def test_install(self):
 		subprocess.check_call('./atxpkg install atxpkg --yes --prefix=/tmp/atxpkg_dest', shell=True)
 		subprocess.check_call('./atxpkg update atxpkg..router --yes --prefix=/tmp/atxpkg_dest', shell=True)
 		subprocess.check_call('./atxpkg remove router --yes --prefix=/tmp/atxpkg_dest', shell=True)
-	#enddef
-#endclass
 
 
 class TestCase(unittest.TestCase):
@@ -44,12 +39,10 @@ class TestCase(unittest.TestCase):
 		self.d = '/tmp/atxpkg'
 		if os.path.isdir(self.d): shutil.rmtree(self.d)
 		os.mkdir(self.d)
-	#enddef
 
 	def tearDown(self):
 		if os.path.isdir(self.d): shutil.rmtree(self.d)
 		if os.path.isdir('/tmp/atxpkg_dest'): shutil.rmtree('/tmp/atxpkg_dest')
-	#enddef
 
 	# TODO: this is hardly finished
 	def test_install_update_remove(self):
@@ -59,8 +52,6 @@ class TestCase(unittest.TestCase):
 		installed_packages[get_package_name(fn)] = installed_package
 		update_package(fn, get_package_name(fn), installed_package, self.d)
 		remove_package(get_package_name(fn), installed_packages, self.d)
-	#enddef
-#endclass
 
 
 class UtilsCase(unittest.TestCase):
@@ -72,7 +63,6 @@ class UtilsCase(unittest.TestCase):
 		self.assertEqual(get_package_name('package-name-3.5.6-1.atxpkg.zip'), 'package-name')
 		self.assertEqual(get_package_name('package-name-3.5.6-1'), 'package-name')
 		self.assertEqual(get_package_name('package-name'), 'package-name')
-	#enddef
 
 	def test_package_version(self):
 		self.assertEqual(get_package_version('package-3.5.6-1.atxpkg.zip'), '3.5.6-1')
@@ -80,7 +70,6 @@ class UtilsCase(unittest.TestCase):
 
 		self.assertEqual(get_package_version('package'), None)
 		self.assertEqual(get_package_version('package-with-hyphen'), None)
-	#enddef
 
 	def test_valid_package_fn(self):
 		self.assertTrue(is_valid_package_fn('package-3.5.6-6.atxpkg.zip'))
@@ -94,7 +83,6 @@ class UtilsCase(unittest.TestCase):
 		self.assertFalse(is_valid_package_fn('package-xxx-6.atxpkg.zip'))
 		self.assertFalse(is_valid_package_fn('package-name-xxx-xxx.atxpkg.zip'))
 		self.assertFalse(is_valid_package_fn('package-name-3-6.xxx.zip'))
-	#enddef
 
 	def test_max_version(self):
 		urls = [
@@ -106,10 +94,7 @@ class UtilsCase(unittest.TestCase):
 		]
 
 		self.assertEqual(get_max_version_url(urls), 'http://example.com/repo/package-2.2-3.atxpkg.zip')
-	#enddef
-#endclass
 
 
 if __name__ == '__main__':
 	unittest.main()
-#endif
