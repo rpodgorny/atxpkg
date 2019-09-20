32a660ee48c7cff4e2450efc8e809ffa459fb79d
Radek Podgorny <radek@podgorny.cz>
2019-09-20 16:23:37 +0200

    better output

--- atxpkg.py
+++ atxpkg.py
@@ -241,14 +241,15 @@ def main():
 		if packages:
 			err = 0
 			for package in packages:
+				print('checking %s' % package)
 				for fn in installed_packages[package]['md5sums']:
 					if not os.path.isfile('%s/%s' % (prefix, fn)):
-						print('%s/%s does not exist' % (prefix, fn))
+						print('%s: %s/%s does not exist' % (package, prefix, fn))
 						err = 1
 					elif fn in installed_packages[package]['backup']:
 						pass  # ignore config files
 					elif utils.get_md5sum('%s/%s' % (prefix, fn)) != installed_packages[package]['md5sums'][fn]:
-						print('sum of %s/%s differs' % (prefix, fn))
+						print('%s: sum of %s/%s differs' % (package, prefix, fn))
 						err = 1
 				print('check of %s complete' % package)
 			if err:
