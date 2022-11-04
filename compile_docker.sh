f54b5f7f63c4e7ae73071de35200a605beb930cc
Radek Podgorny <radek@podgorny.cz>
2022-11-04 11:20:39 +0100

    migrate compilation to podman

--- compile_docker.sh
+++ compile_docker.sh
@@ -5,5 +5,6 @@ trap 'rm -rf "$TMPDIR"' EXIT
 TMPDIR=$(mktemp -d -p /var/tmp) || exit 1
 
 cp -av . ${TMPDIR}
-docker run --rm -it -v ${TMPDIR}:/x rpodgorny/winepybuilder /bin/bash -c "cd /x; ./compile_win.sh"
+#docker run --rm -it -v ${TMPDIR}:/x docker.io/rpodgorny/winepybuilder /bin/bash -c "cd /x; ./compile_win.sh"
+podman run --rm -it -v ${TMPDIR}:/x docker.io/rpodgorny/winepybuilder /bin/bash -c "cd /x; ./compile_win.sh"
 cp -av ${TMPDIR}/dist ./
