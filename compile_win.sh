317d8016bffd6d44b2a636459b8532bc5c9989a3
Radek Podgorny <radek@podgorny.cz>
2022-12-05 17:59:06 +0100

    migrate from cx-freeze to pyinstaller

--- compile_win.sh
+++ compile_win.sh
@@ -6,19 +6,12 @@ PYTHON_SITE_PACKAGES="/wine/drive_c/Python39/Lib/site-packages"
 
 rm -rf build dist
 
+pip install --no-cache-dir -i http://${PYPI_CACHE}:3141/root/pypi/+simple/ --trusted-host ${PYPI_CACHE} "pyinstaller==4.10"  # 5.0.1 is being barfed at by windows defender as containing virus (as of 2022-05-15)
 pip install --no-cache-dir -i http://${PYPI_CACHE}:3141/root/pypi/+simple/ --trusted-host ${PYPI_CACHE} -r requirements.txt
-pip install --no-cache-dir -i http://${PYPI_CACHE}:3141/root/pypi/+simple/ --trusted-host ${PYPI_CACHE} cx-freeze
-#python atxmanager/version.py >__v.txt
-#pyinstaller --noconfirm --clean --noupx --windowed -n 4to6serverw 4to6server.py
-#pyinstaller --noconfirm --clean --noupx 4to6server.py
-#pyinstaller --noconfirm --clean --noupx --windowed -n 4to6clientw 4to6client.py
-#pyinstaller --noconfirm --clean --noupx 4to6client.py
-#pyinstaller --noconfirm --clean --noupx --version-file __v.txt --add-data "atxmanager/templates;templates" --add-data "atxmanager/static;static" --add-data "captions.cs;." -n manager_nw manager.py
-#pyinstaller --noconfirm --clean --noupx --add-data "test;test" tests.py
-#rm -rf __v.txt
-python setup.py install --prefix=dist
 
-#./merge_dist.sh
+pyinstaller --noconfirm --clean --noupx atxpkg.py
+
+./merge_dist.sh
 
 #cp -av c:/python37/lib/site-packages/dateutil dist/  # fix for pyinstaller/arrow/dateutil/whatever... bug
 
