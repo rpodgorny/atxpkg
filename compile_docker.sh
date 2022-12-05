a1398ad9ee97fc021cb44a085aae3919cddcc211
Radek Podgorny <radek@podgorny.cz>
2022-12-05 17:24:54 +0100

    compile in non-interactive mode

--- compile_docker.sh
+++ compile_docker.sh
@@ -5,6 +5,5 @@ trap 'rm -rf "$TMPDIR"' EXIT
 TMPDIR=$(mktemp -d -p /var/tmp) || exit 1
 
 cp -av . ${TMPDIR}
-#docker run --rm -it -v ${TMPDIR}:/x docker.io/rpodgorny/winepybuilder /bin/bash -c "cd /x; ./compile_win.sh"
-podman run --rm -it -v ${TMPDIR}:/x docker.io/rpodgorny/winepybuilder /bin/bash -c "cd /x; ./compile_win.sh"
+podman run --rm -v ${TMPDIR}:/x docker.io/rpodgorny/winepybuilder /bin/bash -c "cd /x; ./compile_win.sh"
 cp -av ${TMPDIR}/dist ./
