16e50d92f2be526ff5f4cdba46f4be9212405103
Radek Podgorny <radek@podgorny.cz>
2023-03-20 17:00:45 +0100

    compilation updates

--- compile_docker.sh
+++ compile_docker.sh
@@ -5,5 +5,5 @@ trap 'rm -rf "$TMPDIR"' EXIT
 TMPDIR=$(mktemp -d -p /var/tmp) || exit 1
 
 cp -av . ${TMPDIR}
-podman run --rm -v ${TMPDIR}:/x docker.io/rpodgorny/winepybuilder /bin/bash -c "cd /x; ./compile_win.sh"
+podman run --rm -it -v ${TMPDIR}:/x docker.io/rpodgorny/winepybuilder:3.11.2-1 /bin/bash -c "cd /x; ./compile_win.sh"
 cp -av ${TMPDIR}/dist ./
--- compile_win.sh
+++ compile_win.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 set -e -x
 
-PYPI_CACHE="172.17.0.1"
-PYTHON_SITE_PACKAGES="/wine/drive_c/Python39/Lib/site-packages"
+PYPI_CACHE="host.containers.internal"
 
 rm -rf build dist
 
-pip install --no-cache-dir -i http://${PYPI_CACHE}:3141/root/pypi/+simple/ --trusted-host ${PYPI_CACHE} "pyinstaller==4.10"  # 5.0.1 is being barfed at by windows defender as containing virus (as of 2022-05-15)
+pip install --no-cache-dir -i http://${PYPI_CACHE}:3141/root/pypi/+simple/ --trusted-host ${PYPI_CACHE} "pyinstaller==5.8.0"
 pip install --no-cache-dir -i http://${PYPI_CACHE}:3141/root/pypi/+simple/ --trusted-host ${PYPI_CACHE} -r requirements.txt
 
 pyinstaller --noconfirm --clean --noupx atxpkg.py
 
 ./merge_dist.sh
 
-#cp -av c:/python37/lib/site-packages/dateutil dist/  # fix for pyinstaller/arrow/dateutil/whatever... bug
-
-cp -av api-ms-win-core-path-l1-1-0.dll dist/  # fix for python3.9 not being supported on windows7
-
-chmod -R a+rwX .  # this is being run under root in docker - so make it possible to delete this dir from outside by regular user
+#cp -av api-ms-win-core-path-l1-1-0.dll dist/  # fix for python3.9 not being supported on windows7
