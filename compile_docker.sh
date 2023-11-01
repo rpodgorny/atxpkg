9048b9e11a5df4c2fe9e41c80e96885841dd323c
Radek Podgorny <radek@podgorny.cz>
2023-11-01 18:15:45 +0800

    gitignore updates

--- compile_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e -x
-
-trap 'rm -rf "$TMPDIR"' EXIT
-TMPDIR=$(mktemp -d -p /var/tmp) || exit 1
-
-cp -av . ${TMPDIR}
-podman run --rm -it -v ${TMPDIR}:/x docker.io/rpodgorny/winepybuilder:3.11.6-1 /bin/bash -c "cd /x; ./compile_win.sh"
-cp -av ${TMPDIR}/dist ./
