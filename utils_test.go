52594d0c1e2b80cc2abb2fb1299f33c09d055682
Radek Podgorny <radek@podgorny.cz>
2023-12-07 14:35:46 +0800

    shuffle tests a bit

---
+++ utils_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"testing"
+)
+
+func TestYesNo(t *testing.T) {
+	tests := map[string]struct {
+		msg  string
+		def  string
+		want bool
+	}{
+		"yes": {
+			msg:  "ahoj",
+			def:  "y",
+			want: true,
+		},
+		"no": {
+			msg:  "ahoj",
+			def:  "n",
+			want: false,
+		},
+		// TODO: this loops forever
+		/*"nothing": {
+			msg: "ahoj",
+			def: "",
+			want: false,
+		},*/
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			ans := YesNo(tt.msg, tt.def)
+			if ans != tt.want {
+				t.Errorf("got %v, want %v", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetRecursiveListing(t *testing.T) {
+	tmpDir := t.TempDir()
+	if err := os.MkdirAll(tmpDir+"/some/directory", os.ModePerm); err != nil {
+		t.Fatalf(fmt.Sprintf("%+v", err))
+	}
+	if _, err := os.Create(tmpDir+"/some/file1"); err != nil {
+		t.Fatalf(fmt.Sprintf("%+v", err))
+	}
+	if _, err := os.Create(tmpDir+"/some/directory/file2"); err != nil {
+		t.Fatalf(fmt.Sprintf("%+v", err))
+	}
+	dirs, files, err := GetRecursiveListing(tmpDir)
+	if err != nil {
+		t.Fatalf(fmt.Sprintf("%+v", err))
+	}
+	if !slices.Equal(dirs, []string{"some", "some/directory"}) {
+		t.Errorf("wrong dirs: %v", dirs)
+	}
+	slices.Sort(files)
+	slices.Reverse(files)
+	if !slices.Equal(files, []string{"some/file1", "some/directory/file2"}) {
+		t.Errorf("wrong files: %v", files)
+	}
+}
+
+func TestGetAvailablePackages(t *testing.T) {
+	// TODO: make it work offline
+	packages := GetAvailablePackages(
+		[]string{
+			"http://atxpkg.asterix.cz",
+			"http://atxpkg-dev.asterix.cz",
+			"./test_data",
+		},
+		false,
+	)
+	// TODO: this does not test anything
+	fmt.Printf("%v", packages)
+}
+
