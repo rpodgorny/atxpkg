2fac16eb0555dfadfb6d7d8d74835fc8461bc0c4
Radek Podgorny <radek@podgorny.cz>
2024-03-14 23:33:00 +0100

    improve tests

--- atxpkg_test.go
+++ atxpkg_test.go
@@ -3,25 +3,29 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/gookit/goutil/fsutil"
+	"github.com/samber/lo"
 )
 
 func TestInstallPackage(t *testing.T) {
 	tmpDir := t.TempDir()
-	installedPackage, err := InstallPackage(
+	installedPackage := lo.Must(InstallPackage(
 		"./test_data/atx300-base-6.3-1.atxpkg.zip",
 		tmpDir,
 		false,
-	)
-	if err != nil {
-		t.Fatalf(fmt.Sprintf("%+v", err))
-	}
+	))
+
 	// TODO: not really much testing going on here
 	fmt.Println(installedPackage)
+
+	lo.Must0(installedPackage.Version == "6.3-1")
+	lo.Must0(fsutil.FileExists(tmpDir + "/atx300/memsh.mem"))
 }
 
 func TestUpdatePackage(t *testing.T) {
 	tmpDir := t.TempDir()
-	installedPackage, err := UpdatePackage(
+	installedPackage := lo.Must(UpdatePackage(
 		"./test_data/atx300-base-6.3-1.atxpkg.zip",
 		"atx300-base",
 		InstalledPackage{
@@ -31,10 +35,34 @@ func TestUpdatePackage(t *testing.T) {
 		},
 		tmpDir,
 		false,
-	)
-	if err != nil {
-		t.Fatalf(fmt.Sprintf("%+v", err))
-	}
+	))
+
 	// TODO: not really much testing going on here
 	fmt.Println(installedPackage)
+
+	lo.Must0(installedPackage.Version == "6.3-1")
+	lo.Must0(fsutil.FileExists(tmpDir + "/atx300/memsh.mem"))
+}
+
+func TestUpdatePackageWithBackup(t *testing.T) {
+	tmpDir := t.TempDir()
+	installedPackage := lo.Must(InstallPackage(
+		"./test_data/atx300-base-6.3-1.atxpkg.zip",
+		tmpDir,
+		false,
+	))
+	lo.Must0(fsutil.WriteFile(tmpDir+"/atx300/set/base/base.ini", []byte("test"), 0644))  // protected file
+	lo.Must0(fsutil.WriteFile(tmpDir+"/atx300/memsh.mem", []byte("test"), 0644))  // unprotected file
+	installedPackage = lo.Must(UpdatePackage(
+		"./test_data/atx300-base-6.3-1.atxpkg.zip",
+		"atx300-base",
+		*installedPackage,
+		tmpDir,
+		false,
+	))
+
+	lo.Must0(fsutil.FileExists(tmpDir + "/atx300/set/base/base.ini"))
+	lo.Must0(fsutil.FileExists(tmpDir + "/atx300/set/base/base.ini.atxpkg_new"))
+	lo.Must0(fsutil.FileExists(tmpDir + "/atx300/memsh.mem"))
+	lo.Must0(!fsutil.FileExists(tmpDir + "/atx300/memsh.mem.atxpkg_new"))
 }
